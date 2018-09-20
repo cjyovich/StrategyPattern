89c8c75e28af9bd64aef6e52a5ac19d36ad63afa
Jessica Jorgenson <jorgenson.jess@gmail.com>
2018-09-20 00:43:26 -0600

    Added merge, formatted

--- ClientTester.java
+++ ClientTester.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class ClientTester {
 
 	public static void main(String[] args) {
-		Client client = new Mathmatica(); //default client
+		Client client = new Mathmatica(); // default client
 		System.out.println("Hello! Welcome to your number sorting portal.");
 		System.out.println("Here's the array of numbers we will be sorting.");
 		System.out.println("{5, 23, 17, 2, 8, 20, 52, 31}");
@@ -11,27 +11,28 @@ public class ClientTester {
 		System.out.println("1. Mathmatica");
 		System.out.println("2. MTool");
 		System.out.println("3. MyMath");
+		@SuppressWarnings("resource")
 		Scanner scanner = new Scanner(System.in);
 		int choice = scanner.nextInt();
-		switch(choice) {
-			case 1:
-				System.out.println("You have chosen Mathmatica.");
-				client = new Mathmatica();
-				client.performSort();
-				//print the sorted array
-				break;
-			case 2:
-				System.out.println("You have chosen MTool.");
-				client = new MTool();
-				client.performSort();
-				//print the sorted array
-				break;
-			case 3: 
-				System.out.println("You have chosen MyMath.");
-				client = new MyMath();
-				client.performSort();
-				//print the sorted array
-				break;
+		switch (choice) {
+		case 1:
+			System.out.println("You have chosen Mathmatica.");
+			client = new Mathmatica();
+			client.performSort();
+			// print the sorted array
+			break;
+		case 2:
+			System.out.println("You have chosen MTool.");
+			client = new MTool();
+			client.performSort();
+			// print the sorted array
+			break;
+		case 3:
+			System.out.println("You have chosen MyMath.");
+			client = new MyMath();
+			client.performSort();
+			// print the sorted array
+			break;
 		}
 		System.out.println("Great! Now let's try sorting with insertion sort.");
 		System.out.println("Here the unsorted array:");
